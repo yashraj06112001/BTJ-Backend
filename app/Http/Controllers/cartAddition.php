75509de68561a6d269c561fcf27020822122a5a2
yash raj mishra <yashraj06112001@gmail.com>
2025-04-07 15:51:33 +0530

    welcome route is created

--- app/Http/Controllers/cartAddition.php
+++ app/Http/Controllers/cartAddition.php
@@ -9,13 +9,5 @@ class cartAddition extends Controller
     //
     public function cartAdding(Request $request)
     {
-     $name=$request->name;
-     $qty=$request->quantity;
-     $price=$request->price;
-     $totalPrice=$price*$qty;
-     if($totalPrice)
-     {
-        
-     }   
     }
 }
